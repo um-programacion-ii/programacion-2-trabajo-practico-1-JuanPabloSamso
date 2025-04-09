9f7f4bce99171b57fe0c6626ce78dc2d6254350f
JuanPabloSamso <j.samso@alumno.um.edu.ar>
2025-04-08 21:02:20 -0300

    Se agregan getters ,setters y método mostrarInformacion()

--- src/vehiculos/Vehiculo.java
+++ src/vehiculos/Vehiculo.java
@@ -26,5 +26,53 @@ public class Vehiculo {
         this.capacidadCargaKg = capacidadCargaKg;
     }
 
+    // Getters y Setters
+    public String getPatente() {
+        return patente;
+    }
 
+    public void setPatente(String patente) {
+        if (patente == null || patente.isEmpty()) {
+            throw new IllegalArgumentException("La patente no puede ser nula o vacía");
+        }
+        this.patente = patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        int anioActual = java.time.Year.now().getValue();
+        if (anio < 1900 || anio > anioActual) {
+            throw new IllegalArgumentException("El año debe estar entre 1900 y " + anioActual);
+        }
+        this.anio = anio;
+    }
+
+    public double getCapacidadCargaKg() {
+        return capacidadCargaKg;
+    }
+
+    public void setCapacidadCargaKg(double capacidadCargaKg) {
+        if (capacidadCargaKg <= 0) {
+            throw new IllegalArgumentException("La capacidad de carga debe ser mayor a cero");
+        }
+        this.capacidadCargaKg = capacidadCargaKg;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Patente: " + this.patente);
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Año: " + this.anio);
+        System.out.println("Capacidad de carga (Kg): " + this.capacidadCargaKg);
+    }
 }
