25902ba942901129d885d0229f3e8cf505af5526
JuanPabloSamso <j.samso@alumno.um.edu.ar>
2025-04-09 02:51:40 -0300

    Se agrega la gestión de vehículos mediante un ArrayList en Main

--- src/vehiculos/Main.java
+++ src/vehiculos/Main.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear instancias de diferentes tipos de vehículos
+        // Crear instancias de cada tipo de vehículo
         Vehiculo vehiculo1 = new Vehiculo("XYZ321", "Renault", 2016, 800.0);
         Vehiculo camion1 = new Camion("CAM001", "Volvo", 2018, 1200.0, true);
         Vehiculo auto1 = new Auto("AUTO001", "Audi", 2020, 600.0, 5);
 
-        // Almacenar las instancias en un ArrayList de tipo Vehiculo
+        // Crear un ArrayList para gestionar de forma dinámica los vehículos
         ArrayList<Vehiculo> vehiculos = new ArrayList<>();
         vehiculos.add(vehiculo1);
         vehiculos.add(camion1);
         vehiculos.add(auto1);
 
-        // Instanciar el impresor de vehículos
+        // Instanciar VehiculoPrinter para imprimir la información de cada vehículo
         VehiculoPrinter printer = new VehiculoPrinter();
 
         // Recorrer la lista e imprimir la información de cada vehículo
